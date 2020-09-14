98e5b6e4a070db251f0900985700e051e631a712
lburgos <luisa.burgos@yopmail.com>
2020-09-14 11:14:42 -0500

    init proyecto

---
+++ src/main/java/com/qdit/ServiciosProgramaApplication.java
@@ -0,0 +1,13 @@
+package com.qdit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosProgramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosProgramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qdit/ServiciosProgramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosProgramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
